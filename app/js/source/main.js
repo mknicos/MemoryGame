3cc9b1698c25f52f6b006abef215108b7f340f0f
Matt Knicos <mknicos@gmail.com>
2014-01-25 07:50:57 -0600

    Random Numbers and Cards Generated

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -3,10 +3,36 @@
 (function(){
   'use strict';
 
+ //var cards = [];
+//  for(var i = 0; i < 26; i ++){
+  //  cards.push(i);
+  //
+ // }
+  var startcards = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
   $(document).ready(init);
 
   function init(){
+    $('#play').click(clickStartGame);
 
+  }
+
+  function clickStartGame(){
+    generateRandomCards();
+  }
+
+  function generateRandomCards(){
+    debugger;
+    var playingCards = [];
+    while(playingCards.length < 20){
+      var randomIndex = Math.floor(Math.random()* 26);
+      var select = startcards[randomIndex];
+      if( playingCards.indexOf(select) === -1){
+        playingCards.push(select);
+        playingCards.push(select);
+      }
+      console.log(playingCards);
+    }
   }
 
 
